b64b96ba037f8a65cc0f06f6a5d0bd464fd81c84
Andry T <andryjosephtumacole@gmail.com>
2024-10-02 01:09:51 +0800

    created permit card components and built the basic functionality of the page

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,24 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App.jsx'
-
+import App from "./App.jsx";
+import AdminPermits from "./pages/AdminPermits.jsx";
+import PermitCard from "./components/PermitCard.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App />,
   },
+  {
+    path: "/manage",
+    element: <AdminPermits />,
+  },
+  {
+    path: "/permit",
+    element: <PermitCard />,
+  },
+]);
 
-
-])
-
-createRoot(document.getElementById('root')).render(
-    <RouterProvider router = {router}/>
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
--- src/pages/AdminPermits.jsx
+++ src/pages/AdminPermits.jsx
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import "../index.css";
 import { Button } from "@/components/ui/button";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
-const AdmintPermits = () => {
-  return <></>;
+import PermitCard from "@/components/PermitCard";
+import permitsMockData from "@/utils/mockData";
+
+const AdminPermits = () => {
+  const [permitType, setPermitType] = useState("Late Night");
+  const handleChangeView = (viewType) => {
+    if (permitType != viewType) {
+      setPermitType(viewType);
+    }
+  };
+  return (
+    <>
+      <ResizablePanelGroup
+        className="max-w-md rounded-lg border md:min-w-[450px] bg-slate-300"
+        direction="horizontal"
+        style={{ margin: "50px" }}
+      >
+        <ResizablePanel
+          className="flex justify-center align-middle"
+          defaultSize={33}
+        >
+          <Button
+            className="m-1"
+            onClick={() => handleChangeView("Late Night")}
+          >
+            Late Night
+          </Button>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel
+          className="flex justify-center align-middle"
+          defaultSize={33}
+        >
+          <Button className="m-1" onClick={() => handleChangeView("Overnight")}>
+            Overnight
+          </Button>
+        </ResizablePanel>
+        <ResizableHandle />
+
+        <ResizablePanel
+          className="flex justify-center align-middle"
+          defaultSize={33}
+        >
+          <Button className="m-1" onClick={() => handleChangeView("Weekend")}>
+            Weekend
+          </Button>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+      <ul>
+        {permitsMockData
+          .filter((permit) => permit.type == permitType)
+          .map((permit) => (
+            <li key={permit.id}>
+              <PermitCard
+                name={permit.name}
+                room={permit.room}
+                status={permit.status}
+              />
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 
 export default AdminPermits;

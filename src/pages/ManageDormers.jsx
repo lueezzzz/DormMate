469553f7ff46e669e34a2e35d3263eaf09a81c06
Andry T <andryjosephtumacole@gmail.com>
2024-12-10 02:09:16 +0800

    removed hard-coded uid for testing

--- src/pages/ManageDormers.jsx
+++ src/pages/ManageDormers.jsx
@@ -83,8 +83,7 @@ const ManageDormers = () => {
 
   const handleRemoveDormer = async (dormerUID) => {
     try {
-      // await removeDormerByUID(dormerUID);
-      await removeDormerByUID("TW2QtbSWxvez6HXZ2hDG5uJ5BQu2");
+      await removeDormerByUID(dormerUID);
       console.log("Removed dormer");
     } catch {
       console.log("Error removing");

9c07777a55dbc66731e90c4d23381d1a9492b37c
rianluisx <rianmabait45@gmail.com>
2025-05-26 21:03:37 +0800

    cypress logout modifications

--- cypress/e2e/logout/logout.js
+++ cypress/e2e/logout/logout.js
@@ -1,5 +1,10 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+
+Given("the DormMate app is running", () => {
+  cy.visit("/");
+});
+
 Given(
   "I am logged in as a dormer with email {string} and password {string}",
   (email, password) => {
@@ -7,29 +12,24 @@ Given(
     cy.get("#email").clear().type(email);
     cy.get("#password").clear().type(password);
     cy.get('button[type="submit"]').click();
-    cy.location("pathname", { timeout: 10000 }).should("eq", "/file-permit");
+    cy.location("pathname", { timeout: 10_000 }).should("eq", "/file-permit");
   }
 );
 
 When("I click the logout button", () => {
-
   cy.contains("button", "Log Out").click();
 });
 
 Then("I should see a logout confirmation modal", () => {
-
   cy.contains("Are you sure you want to log out?").should("be.visible");
 });
 
-When("I confirm the logout", () => {
 
+When("I confirm the logout", () => {
   cy.contains("button", "Confirm").click();
 });
 
-Then("I should be redirected to {string}", (path) => {
-  cy.location("pathname", { timeout: 10000 }).should("eq", path);
-});
 
-Then("I should see {string}", (text) => {
-  cy.contains(text).should("be.visible");
+Then("I should be redirected to {string}", (path) => {
+  cy.location("pathname", { timeout: 10_000 }).should("eq", path);
 });
